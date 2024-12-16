f25668d3708960ffc4b4709eebf515efa9754808
SyedMujtabahz <63043189+SyedMujtabahz@users.noreply.github.com>
2024-12-16 19:37:05 +0500

    Added files

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// GSAP Animations
+gsap.from("#header h1", { opacity: 0, y: -50, duration: 1 });
+gsap.from(".subtitle", { opacity: 0, y: 50, duration: 1, delay: 0.5 });
+gsap.from(".content", { opacity: 0, scale: 0.9, duration: 1, delay: 1 });
