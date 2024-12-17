63266ebc31000329266bdeb1b678c35f83f313f4
BehzadShahid319 <behzadshahid95@gmail.com>
2024-12-17 17:12:33 +0500

    Added animated Backgrounds and Updated Portfolio Cards Data

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // GSAP Animations
-gsap.from("#header h1", { opacity: 0, y: -50, duration: 1 });
-gsap.from(".subtitle", { opacity: 0, y: 50, duration: 1, delay: 0.5 });
-gsap.from(".content", { opacity: 0, scale: 0.9, duration: 1, delay: 1 });
+gsap.from("#header h1", { opacity: 0, y: -50, duration: 0.5 });
+gsap.from(".subtitle", { opacity: 0, y: 50, duration: 0.5, delay: 0.5 });
+gsap.from(".content", { opacity: 0, scale: 0.9, duration: 0.5, delay: 0.5 });
 
 function scrollCarousel(direction) {
     const container = document.querySelector('.carousel-container');
